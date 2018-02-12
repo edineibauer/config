0c49dccb8a93f5bd36b7de79d7cce35964c24129
Edinei <galera.org@gmail.com>
2018-02-12 19:38:45 -0200

    config changes and images upload

--- view/checkDependencies.php
+++ view/checkDependencies.php
@@ -14,5 +14,5 @@ foreach (Helper::listFolder(PATH_HOME . "vendor/conn") as $item) {
 
 if (!$config && file_exists(PATH_HOME . "vendor/conn/config/ajax/defecon4.php"))
     include_once PATH_HOME . "vendor/conn/config/ajax/defecon4.php";
-
-header("Location: " . HOME . "dashboard");
+elseif(!$config)
+    header("Location: " . HOME . "dashboard");

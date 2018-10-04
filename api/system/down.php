b67cf8c0dfcd21ba873cde3cd105f5769ccbb022
Edinei <galera.org@gmail.com>
2018-10-04 14:31:39 -0300

    remove controle fixo de bibliotecas PHP ao desvincular site do controler

--- api/system/down.php
+++ api/system/down.php
@@ -13,6 +13,22 @@ try {
         fclose($f);
     }
 
+    //remove composer biblioteca controle fixo
+    if(file_exists(PATH_HOME . "composer.json")) {
+        $comp = json_decode(file_get_contents(PATH_HOME . "composer.json"), true);
+        foreach ($comp['require'] as $lib => $version) {
+            if(preg_match('/^conn\//i', $lib)) {
+                $v = explode('.', $version);
+                $comp['require'][$lib] = $v[0] . '.' . $v[1] . ".*";
+            }
+        }
+
+        //Salva composer
+        $f = fopen(PATH_HOME . "composer.json", "w");
+        fwrite($f, json_encode($comp));
+        fclose($f);
+    }
+
     $data['error'] = "";
 
 } catch (Exception $e) {

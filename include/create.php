1ec1979ccf8b3be7d1b989237d786e0e0ba85dbd
Edinei <galera.org@gmail.com>
2018-10-09 09:09:43 -0300

    new redirect after create

--- include/create.php
+++ include/create.php
@@ -174,7 +174,7 @@ if (!empty($dados['sitename']) && !empty($_FILES['favicon']['name'])) {
 
         Config\Config::createHtaccess($dados['dominio'], $dados['www'], $dados['ssl']);
 
-        header("Location: ../../../updateSystem");
+        header("Location: ../../../dashboard/updateSystem");
     } else {
         echo "<h3 class='container' style='text-align:center;padding-top:30px;color:red'>Erro ao se Comunicar com o Banco de Dados</h3>";
         require_once 'form.php';
